330ed482e6991012c342f2e90c502e895e1cf171
Mohamed <mohameds3m40780392@gmail.com>
2026-06-07 19:51:31 +0300

    fix admin middleware closure

--- app/Http/Middleware/CheckIsAdmin.php
+++ app/Http/Middleware/CheckIsAdmin.php
@@ -4,23 +4,16 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class CheckIsAdmin
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  Closure(Request): (Response)  $next
-     */
     public function handle(Request $request, Closure $next)
     {
-        if ($request->user() && $request->user()->role === 'admin') {
-            return $next($request);
+        if ($request->user()->role !== 'admin') {
+            return response()->json([
+                'message' => 'Unauthorized access. Admins only.'
+            ], 403);
         }
-
-        return response()->json([
-            'message' => 'Unauthorized access. Admins only.'
-        ], 403);
+        return $next($request);
     }
 }
